dc6dcf86b9282d6d583a9b4e783a300c157c9d30
PoolJK <jkrein@gmx.de>
2019-04-09 16:18:29 +0200

    different command on raspy

--- software/raspy/table.py
+++ software/raspy/table.py
@@ -65,7 +65,10 @@ class Table:
         # binary = cv2.bitwise_not(gray)
         # cv2.imshow("thresh", thresh)
         # _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if settings.on_raspy:
+            _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
             (x, y), (w, h), angle = cv2.minAreaRect(contour)
