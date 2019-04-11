fc95bc9b15c874e543669857b3ea236c05cb9d9f
Marius Knötel <marius.knoetel@sc147essen.de>
2019-04-12 01:11:42 +0200

    after merge cleanup main.py runs billardtrainer.py runs

--- software/raspy/main.py
+++ software/raspy/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import argparse
-import sys
 import cv2
 import numpy as np
 import settings
@@ -24,14 +23,13 @@ def mouse_callback(event, x, y, flags, param):
 
 
 def main():
-    print("OpenCV version :  {0}".format(cv2.__version__))
-    clParser = argparse.ArgumentParser()
-    clParser.add_argument('-f', '--file', dest="filename", help="image file to process",
-                          required=False)
-    clParser.add_argument('-d', '--debug',  dest="debug", help="show more debug output",
-                          action="store_true", default=False)
-
-    args = clParser.parse_args()
+    print("OpenCV version :  {}.{}".format(cv2.getVersionString(), cv2.getVersionRevision()))
+    cl_parser = argparse.ArgumentParser()
+    cl_parser.add_argument('-f', '--file', dest="filename", help="image file to process",
+                           required=False)
+    cl_parser.add_argument('-d', '--debug',  dest="debug", help="show more debug output",
+                           action="store_true", default=False)
+    args = cl_parser.parse_args()
 
     if args.debug:
         settings.debugging = True
@@ -42,7 +40,6 @@ def main():
         settings.on_raspy = False
     else:
         settings.on_raspy = True
-    
 
     # create window for result output (height, width, dimension for numpy array)
     if settings.on_raspy:
@@ -51,9 +48,11 @@ def main():
     else:
         cv2.namedWindow("result")
 
+    mini_beamer = None
+    capture = None
     # get image from file or camera
     if args.filename:
-        """ Load image from disk if filename is given as parameter"""
+        """ Load image from disk if filename is given as parameter """
         src = cv2.imread(args.filename, cv2.IMREAD_COLOR)
         # Check if image is loaded fine
         if src is None:
@@ -62,8 +61,8 @@ def main():
 
         cv2.imshow("Source Image", src)
     else:
-        miniBeamer = Beamer(1280, 720)
-        """ give out white image with beamer and take picture"""
+        mini_beamer = Beamer(1280, 720)
+        """ give out white image with beamer and take picture """
         # create white image for beamer as light source
         white = np.zeros((IMAGEHEIGHT, IMAGEWIDTH, 3), np.uint8)
         white[:] = (255, 255, 255)
@@ -94,48 +93,49 @@ def main():
     # attach mous callback to window for measuring
     cv2.setMouseCallback("result", mouse_callback)
 
-
     # create black image to show found artefacts in
-    outPict = np.zeros((IMAGEHEIGHT, IMAGEWIDTH, 3 ), np.uint8)
-    outPict[:] = (0, 0, 0)
+    out_pict = np.zeros((IMAGEHEIGHT, IMAGEWIDTH, 3), np.uint8)
+    out_pict[:] = (0, 0, 0)
 
     # create gray image
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     gray = cv2.medianBlur(gray, 5)
 
-    #if args.debug:
+    # if args.debug:
     #   cv2.imshow("gray", gray)
 
-    #find table
+    # find table
     found_table = Table.find(gray)
-    #draw table
+    # draw table
     if found_table is not None:
-        found_table.drawSelf(outPict)
+        found_table.draw_self(out_pict)
+        if settings.debugging:
+            cv2.imshow("Found table", out_pict)
+            print("table: y: {}  y: {}  width: {}  height: {}  angle: {}"
+                  .format(found_table.x, found_table.y, found_table.width, found_table.height, found_table.angle))
 
-    if settings.debugging:
-        cv2.imshow("Found table", outPict)
-        print("table: y: {}  y: {}  width: {}  height: {}  angle: {}"
-              .format(found_table.x, found_table.y, found_table.w, found_table.h, found_table.angle))
-
-    #find balls
+    # find balls
     found_balls = Ball.find(gray)
 
-    #draw balls (inside table)
+    # draw balls (inside table)
     for ball in found_balls:
-        #if(found_table.isInside(ball)):
-        ball.draw(outPict)
+        # if(found_table.isInside(ball)):
+        ball.draw(out_pict)
 
-    if settings.on_raspy:
-        cv2.imshow("result", miniBeamer.getImage(found_table, outPict))
+    if settings.on_raspy and found_table is not None:
+        cv2.imshow("result", mini_beamer.get_image(found_table, out_pict))
     else:
-        cv2.imshow("result", outPict)
+        cv2.imshow("result", out_pict)
 
     #    if (cv2.waitKey(30) & 0xFF) == 27:
     #       break
 
     cv2.waitKey()
+    if capture is not None:
+        capture.release()
     cv2.destroyAllWindows()
     return 0
+
 
 if __name__ == '__main__':
     main()
