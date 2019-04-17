6b058afb217dee806bc09e5ec6b03e1cae364fba
PoolJK <jkrein@gmx.de>
2019-04-17 09:41:28 +0200

    camera test with live image

--- software/raspy/tools/camera_test.py
+++ software/raspy/tools/camera_test.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
-from picamera import PiCamera
-from time import sleep
+"""
+Simply display the contents of the webcam with optional mirroring using OpenCV 
+via the new Pythonic cv2 interface.  Press <esc> to quit.
+"""
 
-camera = PiCamera()
+import cv2
 
-camera.start_preview()
-sleep(10)
-camera.stop_preview()
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    main()
